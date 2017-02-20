7e00ad320f7dd2c6070d92bec5bf8f52af8d2af0
Kai Mast <kaimast@cs.cornell.edu>
2017-02-19 20:42:49 -0500

    Fix simulator under GCC6

--- psw/urts/node.cpp
+++ psw/urts/node.cpp
@@ -37,10 +37,8 @@
 
 template<class T1, class T2>
 Node<T1, T2>::Node(const T1& k, const T2& v)
+    : key(k), value(v), next(nullptr)
 {
-    this->key = k;
-    this->value = v;
-    this->next = NULL;
 }
 
 template<class T1, class T2>
@@ -71,14 +69,11 @@ Node<T1, T2>* Node<T1, T2>::Remove(const T1& k)
 template<class T1, class T2>
 Node<T1, T2>* Node<T1, T2>::Find(const T1& k)
 {
-    Node<T1, T2>* c = this;
-    if (c == NULL) return NULL;
-    while (c != NULL) {
+    for(auto c = this; c != nullptr; c = c->next) {
         if (c->key == k)
-            break;
-        c = c->next;
+            return c;
     }
-    return c;
+    return nullptr;
 }
 
 
