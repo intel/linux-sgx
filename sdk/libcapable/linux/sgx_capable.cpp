9f7938dd46d6b598c31cc3cc7ea2148f40f5a92a
John P Mechalas <john.p.mechalas@intel.com>
2017-06-09 03:33:44 -0700

    Corrected license header to BSD 3-clause

--- sdk/libcapable/linux/sgx_capable.cpp
+++ sdk/libcapable/linux/sgx_capable.cpp
@@ -1,25 +1,32 @@
-/**
-*   Copyright(C) 2011-2017 Intel Corporation All Rights Reserved.
-*
-*   The source code, information  and  material ("Material") contained herein is
-*   owned  by Intel Corporation or its suppliers or licensors, and title to such
-*   Material remains  with Intel Corporation  or its suppliers or licensors. The
-*   Material  contains proprietary information  of  Intel or  its  suppliers and
-*   licensors. The  Material is protected by worldwide copyright laws and treaty
-*   provisions. No  part  of  the  Material  may  be  used,  copied, reproduced,
-*   modified, published, uploaded, posted, transmitted, distributed or disclosed
-*   in any way  without Intel's  prior  express written  permission. No  license
-*   under  any patent, copyright  or  other intellectual property rights  in the
-*   Material  is  granted  to  or  conferred  upon  you,  either  expressly,  by
-*   implication, inducement,  estoppel or  otherwise.  Any  license  under  such
-*   intellectual  property  rights must  be express  and  approved  by  Intel in
-*   writing.
-*
-*   *Third Party trademarks are the property of their respective owners.
-*
-*   Unless otherwise  agreed  by Intel  in writing, you may not remove  or alter
-*   this  notice or  any other notice embedded  in Materials by Intel or Intel's
-*   suppliers or licensors in any way.
+/*
+* Copyright 2011-2017 Intel Corporation
+* 
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+* 
+* 1. Redistributions of source code must retain the above copyright notice,
+* this list of conditions and the following disclaimer.
+* 
+* 2. Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution.
+* 
+* 3. Neither the name of the copyright holder nor the names of its
+* contributors may be used to endorse or promote products derived from
+* this software without specific prior written permission.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+* HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+* TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <sys/stat.h>
