090f9d639508622e12eebc53c1261d56ef60346d
lzha101 <lili.z.zhang@intel.com>
2018-01-26 17:02:10 +0800

    Update download_prebuilt.sh
    Update download script to use prebuilt AEs with security updates.

--- download_prebuilt.sh
+++ download_prebuilt.sh
@@ -33,13 +33,13 @@
 
 top_dir=`dirname $0`
 out_dir=$top_dir
-optlib_name=optimized_libs-2.1.tar
-ae_file_name=prebuilt-ae-2.1.tar
-server_url_path=https://download.01.org/intel-sgx/linux-2.1/
+optlib_name=optimized_libs-2.1.1.tar
+ae_file_name=prebuilt-ae-2.1.1.tar
+server_url_path=https://download.01.org/intel-sgx/linux-2.1.1/
 server_optlib_url=$server_url_path/$optlib_name
 server_ae_url=$server_url_path/$ae_file_name
 optlib_sha256=239cae39f87934d56c4eb919a4702c6ac82c19957b9a8d56c02b10eb4e27f573
-ae_sha256=3dfac14590a74aa860ea88ca9d1e2e112efb300c622e9ea02b3fa4bd69c50b07
+ae_sha256=f95589a69a8a8767815fe3bccf32bac5c9709022f9f32ae1a726b1da7955200b
 rm -rf $out_dir/$optlib_name
 wget $server_optlib_url -P $out_dir 
 if [ $? -ne 0 ]; then
@@ -66,3 +66,4 @@ if [ $? -ne 0 ]; then
 fi
 
 pushd $out_dir;tar -xf $optlib_name;tar -xf $ae_file_name;rm -f $optlib_name;rm -f $ae_file_name;popd
+
