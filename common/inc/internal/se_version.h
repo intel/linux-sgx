21a54a43230db41d7bd44545ffa7da3647b7330d
Jethro Beekman <jethro@fortanix.com>
2020-03-10 10:57:01 +0100

    Undo copyright year changes
    To view actual 2.9 changes, do:
    git diff e0a51c4974fc1207eebc95f400db4fb4c799eba6..THIS_COMMIT_HASH

--- common/inc/internal/se_version.h
+++ common/inc/internal/se_version.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2020 Intel Corporation. All rights reserved.
+ * Copyright (C) 2011-2019 Intel Corporation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -29,7 +29,7 @@
  *
  */
 #define STRFILEVER    "2.9.100.2"
-#define COPYRIGHT      "Copyright (C) 2020 Intel Corporation"
+ * Copyright (C) 2011-2019 Intel Corporation. All rights reserved.
 
 #define UAE_SERVICE_VERSION       "2.2.202.2"
 #define URTS_VERSION              "1.1.106.2"
